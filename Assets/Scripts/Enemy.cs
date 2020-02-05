d2fc1db28390f8648a64ce11d4d39bbe34fa79f1
Fran <frna.vlz@gmail.com>
2020-02-05 07:54:47 -0700

    mover enemigo

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,8 +6,20 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField]
     float moveSpeed = 3f;
+    [SerializeField]
+    float delay;
+    float timer;
+    [SerializeField]
+    Vector2 dir;
 
-    void Update(){
-        transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+    void Update()
+    {
+        transform.Translate(dir * moveSpeed * Time.deltaTime);
+        timer += Time.deltaTime;
+        if(timer >= delay)
+        {
+            timer = 0;
+            dir = dir == Vector2.right ? Vector2.left : Vector2.right;
+        }
     }
 }
